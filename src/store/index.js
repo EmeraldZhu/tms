2af71c5e1be90b3b698daee6399c741d43e15e0a
Emerald Zhu <zhuemerald@gmail.com>
2024-02-12 15:36:37 +0300

    add new 'role' state

--- src/store/index.js
+++ src/store/index.js
@@ -1,18 +1,34 @@
 import { createApp } from 'vue';
 import { createStore } from 'vuex';
-import { auth } from '../firebase';
+import { auth, db } from '../firebase';
+import { getDoc, doc } from 'firebase/firestore';
 
 // Create a new store instance.
 const store = createStore({
     state() {
         return {
-            user: null
+            user: null,
+            role: null, // new state for user role
         };
     },
     mutations: {
         setUser(state, user) {
             state.user = user;
+        },
+        setRole(state, role) { // new mutation for user role
+            state.role = role;
         }
+    },
+    actions: {
+        async fetchUserRole({ commit }, user) { // new action for fetching user role
+            const docSnap = await getDoc(doc(db, 'users', user.uid));
+
+            if (docSnap.exists()) {
+                commit('setRole', docSnap.data().role);
+            } else {
+                console.log('No such document!');
+            }
+        },
     },
     getters: {
         currentUser: state => {
