b9a816ee7227a4cf2c9dd2a3638b8be1e565fca6
Emerald Zhu <zhuemerald@gmail.com>
2024-02-15 12:13:10 +0300

    create showPassword icon

--- src/views/auth/InviteeRegister.vue
+++ src/views/auth/InviteeRegister.vue
@@ -8,9 +8,23 @@
             <input type="email"  v-model="email" placeholder="Email" class="input" name="email" autocomplete="email" readonly>
             <div class="password-container">
                 <input :type="showPassword ? 'text' : 'password'" v-model="password" placeholder="Password" class="input password" name="password" autocomplete="new-password">
-
+                <img :src="showPassword ? EyeOpen : EyeClosed" class="eye-icon" @click="showPassword = !showPassword">
             </div>
             <button type="submit" class="button">Register</button>
         </form>
     </div>
 </template>
+
+<script>
+import { ref, onMounted } from 'vue';
+import { getAuth, createUserWithEmailAndPassword } from 'firebase/auth';
+import { useStore } from 'vuex';
+import { useRouter, useRoute } from 'vue-router';
+import { setDoc, doc, deleteDoc } from 'firebase/firestore';
+import { db } from '@/firebase';
+
+import EyeOpen from '@/assets/eye-open.svg';
+import EyeClosed from '@/assets/eye-closed.svg';
+
+const auth = getAuth();
+</script>
