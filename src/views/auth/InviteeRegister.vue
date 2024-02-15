79c3e4e25760001412e90c07e924c13bcb97e849
Emerald Zhu <zhuemerald@gmail.com>
2024-02-15 16:18:56 +0300

    import getDoc function from firestore

--- src/views/auth/InviteeRegister.vue
+++ src/views/auth/InviteeRegister.vue
@@ -20,7 +20,7 @@ import { ref, onMounted } from 'vue';
 import { getAuth, createUserWithEmailAndPassword } from 'firebase/auth';
 import { useStore } from 'vuex';
 import { useRouter, useRoute } from 'vue-router';
-import { setDoc, doc, deleteDoc } from 'firebase/firestore';
+import { getDoc, setDoc, doc, deleteDoc } from 'firebase/firestore';
 import { db } from '@/firebase';
 
 import EyeOpen from '@/assets/eye-open.svg';
