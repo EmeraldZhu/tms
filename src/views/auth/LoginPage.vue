037f3e0be7bc4f44ce922cfd6d16c83d57352e60
Emerald Zhu <zhuemerald@gmail.com>
2024-03-05 16:23:16 +0300

    Update landlord and tenant dashboard routes

--- src/views/auth/LoginPage.vue
+++ src/views/auth/LoginPage.vue
@@ -91,9 +91,9 @@ const login = async () => {
 function redirectToDashboard(role)
 {
     if (role === 'landlord')
-        router.push('/landlord-dashboard');
+        router.push('/landlord');
     else if (role === 'tenant')
-        router.push('/tenant-dashboard');
+        router.push('/tenant');
     else if (role === 'caretaker')
         router.push('/caretaker-dashboard');
 }
