77c8d2cd2fa568fac2b5993d438a8fb8c81aa745
Emerald Zhu <zhuemerald@gmail.com>
2024-03-05 16:01:29 +0300

    Add `NavPanel` component and register it globally

--- src/main.js
+++ src/main.js
@@ -2,9 +2,14 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router'; // Assuming you have a router setup
 import store from './store';
+import NavPanel from '@/components/NavPanel.vue';
 
-const app = createApp(App)
+const app = createApp(App);
 // createApp(App).use(store).use(router).mount('#app');
-app.use(store)
-app.use(router)
-app.mount('#app')
+
+app.component('NavPanel', NavPanel);
+
+app.use(store);
+app.use(router);
+
+app.mount('#app');
