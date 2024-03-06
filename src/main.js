6cc20015c441f1ab6f1f10b087594a73effd1726
Emerald Zhu <zhuemerald@gmail.com>
2024-03-07 00:02:36 +0300

    Set authentication state persistence to browser session.

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
-import { getAuth, onAuthStateChanged } from 'firebase/auth';
+import { getAuth, onAuthStateChanged, setPersistence, browserSessionPersistence } from 'firebase/auth';
 import store from './store';
 
 import NavPanel from '@/components/NavPanel.vue';
@@ -15,16 +15,24 @@ app.use(router);
 
 const auth = getAuth();
 
-onAuthStateChanged(auth, (user) => {
-    if (user) {
-        store.commit('setUser', user);
-        // fetch and store user role
-        store.dispatch('fetchUserRole', user);
-    } else {
-        // user is signed out
-        store.commit('setUser', null);
-        store.commit('setRole', null);
-    }
-})
+// Set the authentication state persistence
+setPersistence(auth, browserSessionPersistence)
+  .then(() => {
+    // Persistence is set, you can now listen for authentication state changes
+    onAuthStateChanged(auth, (user) => {
+        if (user) {
+            store.commit('setUser', user);
+            // fetch and store user role
+            store.dispatch('fetchUserRole', user);
+        } else {
+            // user is signed out
+            store.commit('setUser', null);
+            store.commit('setRole', null);
+        }
+    });
+  })
+  .catch((error) => {
+    console.error('Error setting authentication state persistence:', error);
+  });
 
 app.mount('#app');
