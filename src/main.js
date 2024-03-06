08f681dcf403689f9a453bfa69a0b62e989489c7
Emerald Zhu <zhuemerald@gmail.com>
2024-03-06 17:31:00 +0300

    set persisting auth state

--- src/main.js
+++ src/main.js
@@ -1,15 +1,30 @@
 import { createApp } from 'vue';
 import App from './App.vue';
-import router from './router'; // Assuming you have a router setup
+import router from './router';
+import { getAuth, onAuthStateChanged } from 'firebase/auth';
 import store from './store';
+
 import NavPanel from '@/components/NavPanel.vue';
 
 const app = createApp(App);
-// createApp(App).use(store).use(router).mount('#app');
 
 app.component('NavPanel', NavPanel);
 
 app.use(store);
 app.use(router);
+
+const auth = getAuth();
+
+onAuthStateChanged(auth, (user) => {
+    if (user) {
+        store.commit('setUser', user);
+        // fetch and store user role
+        store.dispatch('fetchUserRole', user);
+    } else {
+        // user is signed out
+        store.commit('setUser', null);
+        store.commit('setRole', null);
+    }
+})
 
 app.mount('#app');
